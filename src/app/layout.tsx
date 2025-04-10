5e84bbc4c6ecc6a978aeab8424827a118972ddd2
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-04-11 01:01:20 +0900

    add color mode toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +25,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="size-screen p-2">
+              <div className="relative">
+                <div className="absolute right-0">
+                  <ModeToggle />
+                </div>
+                {children}
+              </div>
+            </div>
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   );
 }
