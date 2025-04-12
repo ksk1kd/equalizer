a590061de5d8b63d1f1fbf15ef2507b456a06d9f
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-04-12 22:13:01 +0900

    add create new project dialog

--- src/app/@island/page.tsx
+++ src/app/@island/page.tsx
@@ -1,3 +1,37 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function Home() {
-  return <>Island home</>;
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger>Create New Project</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create New Project</DialogTitle>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="project-name" className="text-right">
+                Project Name
+              </Label>
+              <Input id="project-name" className="col-span-3" />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Create</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
 }
