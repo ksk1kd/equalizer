d440c62993441a4a6e857cbe8053dea70e6c64f2
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-04-13 04:28:07 +0900

    adjust projects table

--- src/components/ui/projects-table.tsx
+++ src/components/ui/projects-table.tsx
@@ -1,6 +1,15 @@
 "use client";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { useProjectsContext } from "@/contexts/projects";
 
 export default function ProjectsTable() {
@@ -9,16 +18,35 @@ export default function ProjectsTable() {
   return (
     <>
       <div className="w-100">
-        <Card className="min-h-40 max-h-80">
+        <Card className="pb-10">
           <CardHeader>
             <CardTitle>Projects</CardTitle>
           </CardHeader>
           <CardContent>
-            {projects?.length !== 0 ? (
-              projects?.map((project) => project.name)
-            ) : (
-              <p>No project.</p>
-            )}
+            <ScrollArea className="h-50">
+              {projects?.length !== 0 ? (
+                <>
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead className="text-xs h-8">Name</TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      {projects?.map((project) => (
+                        <TableRow key={project.id}>
+                          <TableCell className="text-base">
+                            {project.name}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </>
+              ) : (
+                <p className="text-sm">No project.</p>
+              )}
+            </ScrollArea>
           </CardContent>
         </Card>
       </div>
