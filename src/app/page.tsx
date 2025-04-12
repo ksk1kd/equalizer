56d9f89d8b8a9d88809495e96cee7429714e05e4
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-04-13 03:28:29 +0900

    change top page UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import CreateProjectDialog from "@/components/ui/create-project-dialog";
 import ProjectsTable from "@/components/ui/projects-table";
 
 export default function Home() {
   return (
     <>
-      <ProjectsTable />
+      <div className="flex flex-col gap-4">
+        <ProjectsTable />
+        <CreateProjectDialog />
+      </div>
     </>
   );
 }
