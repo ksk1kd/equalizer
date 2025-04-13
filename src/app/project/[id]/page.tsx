3dd0d2df643385a08efddbb3bacb80fef706aabc
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-04-14 00:46:36 +0900

    delete island

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,11 +1,3 @@
-import { Island } from "@/components/ui/island";
-
 export default function Project() {
-  return (
-    <>
-      <div className="absolute bottom-5 w-full flex items-center justify-center">
-        <Island>Island</Island>
-      </div>
-    </>
-  );
+  return <></>;
 }
--- src/components/ui/island.tsx
+++
@@ -1,9 +0,0 @@
-import type * as React from "react";
-
-export function Island({ children }: React.ComponentProps<"div">) {
-  return (
-    <div className="min-w-50 py-2 px-5 rounded-full border-gray-100 dark:border-gray-800 border-1 shadow-lg/10 shadow-gray-700 dark:shadow-gray-300 flex items-center justify-center">
-      {children}
-    </div>
-  );
-}
